f4b1ed87885a2bae3e7d2df4a868ee880cca438a
abdelmohcine sebban <mohcines@gmail.com>
2025-05-29 10:20:20 +0100

    ex02: demo de la stateful session

--- src/main/java/ma/medtech/drools/App.java
+++ src/main/java/ma/medtech/drools/App.java
@@ -3,21 +3,22 @@ package ma.medtech.drools;
 import ma.medtech.drools.model.Person;
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
-import org.kie.api.runtime.StatelessKieSession;
+import org.kie.api.runtime.KieSession;
 
 public class App {
     public static void main(String[] args) {
         KieServices kieServices = KieServices.Factory.get();
         KieContainer kieContainer = kieServices.getKieClasspathContainer();
 
-        StatelessKieSession session = kieContainer.newStatelessKieSession("rulesSession");
+        KieSession session = kieContainer.newKieSession("rulesSession");
 
         Person jean = new Person("Jean", 70);
 
         System.out.println("=== Avant exécution : " + jean);
-        session.execute(jean);
+        session.insert(jean);
+        session.fireAllRules();
         System.out.println("**************************");
-        session.execute(jean);
+        session.fireAllRules();
         System.out.println("=== Après exécution : " + jean);
     }
 }
