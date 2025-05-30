9e014257160c1e5cb608ce9e5cf7bc81523e10b3
abdelmohcine sebban <mohcines@gmail.com>
2025-05-30 15:07:32 +0100

    ex06: lock-on-active

--- src/main/java/ma/medtech/drools/App.java
+++ src/main/java/ma/medtech/drools/App.java
@@ -1,6 +1,5 @@
 package ma.medtech.drools;
 
-import ma.medtech.drools.model.Person;
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
@@ -9,11 +8,14 @@ public class App {
     public static void main(String[] args) {
         KieServices ks = KieServices.Factory.get();
         KieContainer kc = ks.getKieClasspathContainer();
-        KieSession ksession = kc.newKieSession("rulesSession");
 
-        Person p1 = new Person("Alice", 70);
-        ksession.insert(p1);
-        ksession.fireAllRules();
-        ksession.dispose();
+        KieSession session = kc.newKieSession("rulesSession");
+        session.getAgenda().getAgendaGroup("validation").setFocus();
+
+        session.insert("A");
+        session.fireAllRules();
+
+        session.insert("B"); // Fait qui satisferait la règle B
+        session.fireAllRules(); // Règle B ne s'active pas !
     }
 }
