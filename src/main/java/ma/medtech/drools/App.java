76460a943eb50def261009d8dd1e1e494fe3d8b0
abdelmohcine sebban <mohcines@gmail.com>
2025-06-03 13:02:34 +0100

    step-trace-mode: Added trace for object insertion

--- src/main/java/ma/medtech/drools/App.java
+++ src/main/java/ma/medtech/drools/App.java
@@ -7,7 +7,11 @@ import java.util.List;
 import org.kie.api.KieServices;
 import org.kie.api.event.rule.AfterMatchFiredEvent;
 import org.kie.api.event.rule.DefaultAgendaEventListener;
+import org.kie.api.event.rule.DefaultRuleRuntimeEventListener;
 import org.kie.api.event.rule.MatchCreatedEvent;
+import org.kie.api.event.rule.ObjectDeletedEvent;
+import org.kie.api.event.rule.ObjectInsertedEvent;
+import org.kie.api.event.rule.ObjectUpdatedEvent;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 
@@ -32,6 +36,24 @@ public class App {
                 System.out.println("[TRACE] Règle exécutée : " + event.getMatch().getRule().getName());
             }
         });
+
+        ksession.addEventListener(new DefaultRuleRuntimeEventListener() {
+            @Override
+            public void objectInserted(ObjectInsertedEvent event) {
+                System.out.println("[TRACE] Objet inséré : " + event.getObject());
+            }
+
+            @Override
+            public void objectUpdated(ObjectUpdatedEvent event) {
+                System.out.println("[TRACE] Objet mis à jour : " + event.getObject());
+            }
+
+            @Override
+            public void objectDeleted(ObjectDeletedEvent event) {
+                System.out.println("[TRACE] Objet supprimé : " + event.getOldObject());
+            }
+        });
+
 
 
         Person p1 = new Person("Alice", 70);
