8713391e26e46348a66ff3b2c300267441783c79
abdelmohcine sebban <mohcines@gmail.com>
2025-06-03 13:26:30 +0100

    step-inspect-facts

--- src/main/java/ma/medtech/drools/App.java
+++ src/main/java/ma/medtech/drools/App.java
@@ -1,9 +1,11 @@
 package ma.medtech.drools;
 
 import ma.medtech.drools.model.Person;
+
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.rule.FactHandle;
 
 public class App {
     public static void main(String[] args) {
@@ -13,6 +15,12 @@ public class App {
 
         Person p1 = new Person("Alice", 70);
         ksession.insert(p1);
+
+        for(FactHandle fh : ksession.getFactHandles()) {
+            Object fact = ksession.getObject(fh);
+            System.out.println("Objet: " + fact);
+        }
+
         ksession.fireAllRules();
         ksession.dispose();
     }
