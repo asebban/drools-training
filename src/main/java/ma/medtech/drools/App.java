518dc578ee1448d73e34932ff5094e367bfa2dbd
abdelmohcine sebban <mohcines@gmail.com>
2025-06-03 13:06:44 +0100

    step-log-audit

--- src/main/java/ma/medtech/drools/App.java
+++ src/main/java/ma/medtech/drools/App.java
@@ -2,6 +2,7 @@ package ma.medtech.drools;
 
 import ma.medtech.drools.model.Person;
 import org.kie.api.KieServices;
+import org.kie.api.logger.KieRuntimeLogger;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 
@@ -10,10 +11,14 @@ public class App {
         KieServices ks = KieServices.Factory.get();
         KieContainer kc = ks.getKieClasspathContainer();
         KieSession ksession = kc.newKieSession("rulesSession");
+        // Audit en fichier texte
+        KieRuntimeLogger logger = ks.getLoggers().newFileLogger(ksession, "log/trace");
+
 
         Person p1 = new Person("Alice", 70);
         ksession.insert(p1);
         ksession.fireAllRules();
+        logger.close();
         ksession.dispose();
     }
 }
