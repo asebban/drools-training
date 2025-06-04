79fed0a1be282edda7bb83d87939ec43f9199aa9
abdelmohcine sebban <mohcines@gmail.com>
2025-06-04 10:31:03 +0100

    step-agenda-group

--- src/main/java/ma/medtech/drools/App.java
+++ src/main/java/ma/medtech/drools/App.java
@@ -12,7 +12,11 @@ public class App {
         KieSession ksession = kc.newKieSession("rulesSession");
 
         Person p1 = new Person("Alice", 70);
+        Person p2 = new Person("Bob", 15);
         ksession.insert(p1);
+        ksession.insert(p2);
+        ksession.getAgenda().getAgendaGroup("senior").setFocus();
+        ksession.getAgenda().getAgendaGroup("junior").setFocus();
         ksession.fireAllRules();
         ksession.dispose();
     }
