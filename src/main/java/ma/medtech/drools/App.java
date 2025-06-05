5d32c2163afe1ed362102de51d9c1aaeaebbea14
abdelmohcine sebban <mohcines@gmail.com>
2025-06-05 12:39:18 +0100

    Ajout de l'update de jean pour vérifier qu'il reste dans la session

--- src/main/java/ma/medtech/drools/App.java
+++ src/main/java/ma/medtech/drools/App.java
@@ -4,6 +4,7 @@ import ma.medtech.drools.model.Person;
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.rule.FactHandle;
 
 public class App {
     public static void main(String[] args) {
@@ -13,11 +14,15 @@ public class App {
         KieSession session = kieContainer.newKieSession("rulesSession");
 
         Person jean = new Person("Jean", 70);
+        Person paul = new Person("Paul", 80);
 
         System.out.println("=== Avant exécution : " + jean);
         session.insert(jean);
         session.fireAllRules();
         System.out.println("**************************");
+        session.insert(paul);
+        FactHandle jeanHandle = session.getFactHandle(jean); // Récupère le FactHandle de Jean
+        session.update(jeanHandle, jean); // Notifie Drools que Jean doit être réévalué
         session.fireAllRules();
         System.out.println("=== Après exécution : " + jean);
     }
