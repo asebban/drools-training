f4a6e32a252e2b67799faa0f425146fbad5b65b4
abdelmohcine sebban <mohcines@gmail.com>
2025-07-01 11:26:46 +0100

    Added client Ali

--- src/main/java/ma/medtech/drools/App.java
+++ src/main/java/ma/medtech/drools/App.java
@@ -17,14 +17,18 @@ public class App {
         
         Client alice = new Client("Alice", 70, "FR", List.of("USER"));
         Client bob   = new Client("Bob", 35, "NG", List.of("ADMIN", "USER"));
+        Client ali   = new Client("Ali", 20, "MA", List.of("USER"));
 
         Transaction t1 = new Transaction(5000.0, "EUR", "FR", alice);
         Transaction t2 = new Transaction(12000.0, "USD", "NG", bob);
+        Transaction t3 = new Transaction(1000.0, "MAD", "MA", ali);
 
         ksession.insert(alice);
         ksession.insert(bob);
+        ksession.insert(ali);
         ksession.insert(t1);
         ksession.insert(t2);
+        ksession.insert(t3);
 
         ksession.fireAllRules();
         ksession.dispose();
